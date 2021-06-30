5bd446f1292e34e88117fd806d76ccbb1dccba50
LuisLimaIt <pls.luis@gmail.com>
2021-06-30 14:53:49 -0300

    Invocando Action Creator #02

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -7,42 +7,43 @@ import { alterarNumeroMinimo } from '../store/actions/numeros';
 
 
 function Intervalo(props) {
-    const { min, max } = props;
+  const { min, max } = props;
 
-    return (
-        <Card title="Intervalo de números" red>
-            <div className="Intervalo">
-                <span>
-                    <strong>Mínimo:</strong>
-                    <input type="number" value={min} readOnly />
-                </span>
-                <span>
-                    <strong>Máximo:</strong>
-                    <input type="number" value={max} readOnly />
-                </span>
-            </div>
-        </Card>
-    )
+  return (
+    <Card title="Intervalo de números" red>
+      <div className="Intervalo">
+        <span>
+          <strong>Mínimo:</strong>
+          <input type="number" value={min} 
+            onChange={e => props.alterarMinimo(+e.target.value)} />
+        </span>
+        <span>
+          <strong>Máximo:</strong>
+          <input type="number" value={max} readOnly />
+        </span>
+      </div>
+    </Card>
+  )
 }
 
 function mapStateToProps(state) {
-    return {
-        min: state.numeros.min,
-        max: state.numeros.max,
-    }
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  }
 } 
 
-function mapActionCreatorsToProp(dispatch) {
-    return {
-        alterarMinimo(novoNumero) {
-            //action creator -> action
-            const action = alterarNumeroMinimo(novoNumero);
-            dispatch(action);
-        },
-    }
+function mapDispatchToProps(dispatch) {
+  return {
+    alterarMinimo(novoNumero) {
+        //action creator -> action
+        const action = alterarNumeroMinimo(novoNumero);
+        dispatch(action);
+    },
+  }
 }
 
 export default connect(
-    mapStateToProps,
-    mapActionCreatorsToProp
+  mapStateToProps,
+  mapDispatchToProps
 )(Intervalo);
